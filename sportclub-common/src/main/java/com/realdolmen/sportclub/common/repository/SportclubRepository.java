d68bc19f6988e86339bb24c7cf12589c984b46b7
Olivier Cappelle <olivier.cappelle@realdolmen.com>
2017-12-05 18:31:38 +0100

    refactor

--- sportclub-common/src/main/java/com/realdolmen/sportclub/common/repository/SportclubRepository.java
+++ sportclub-common/src/main/java/com/realdolmen/sportclub/common/repository/SportclubRepository.java
@@ -4,5 +4,6 @@ import com.realdolmen.sportclub.common.entity.Sportclub;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SportclubRepository extends JpaRepository<Sportclub, Long> {
 }
