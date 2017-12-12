2f75fe9afad29df15979f929c35ceb9f762a505d
Jeroen Bosmans <jeroen.bosmans@realdolmen.com>
2017-12-12 13:40:54 +0100

    merged model in feature branch

--- sportclub-frontend/src/app/sportclub-events/components/sportclub-event-creation/sportclub-event-creation.component.ts
+++ sportclub-frontend/src/app/sportclub-events/components/sportclub-event-creation/sportclub-event-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SportClubEvent} from "../../model/sportclub-event";
 
 @Component({
   selector: 'app-sportclub-event-creation',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sportclub-event-creation.component.css']
 })
 export class SportclubEventCreationComponent implements OnInit {
+
+  event: SportClubEvent;
 
   constructor() { }
 
