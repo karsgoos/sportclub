a160db1047b79b5eac223cc16de0c5aaede9639e
Gust Wittevrongel <gust.wittevrongel@realdolmen.com>
2017-12-12 17:23:04 +0100

    basic controller and repository

--- sportclub-events/src/main/java/com/realdolmen/sportclub/events/controller/EventController.java
+++ sportclub-events/src/main/java/com/realdolmen/sportclub/events/controller/EventController.java
@@ -3,14 +3,12 @@ package com.realdolmen.sportclub.events.controller;
 import com.realdolmen.sportclub.common.entity.Event;
 import com.realdolmen.sportclub.events.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 public class EventController {
@@ -18,11 +16,18 @@ public class EventController {
 	@Autowired
 	private EventRepository eventRepository;
 	
-	@RequestMapping(method = RequestMethod.GET, value = "events/all")
+	@RequestMapping(method = RequestMethod.GET, value = "api/events/all")
 	public Collection<Event> getEvents(){
-		List a = new ArrayList<Event>();
-		a.add(new Event());
-		return a;
-//		return eventRepository.findAll();
+		return eventRepository.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "api/events/test")
+	public Event getEvent(){
+		return new Event();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "api/events/{id}")
+	public Event getEvent(@PathVariable Long id){
+		return eventRepository.findOne(id);
 	}
 }
