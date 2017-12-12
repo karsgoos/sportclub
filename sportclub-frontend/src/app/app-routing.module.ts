4e761ff9bfaa8c505fa4fcc01120f18ff5093f7f
Michael Feyaerts <michael.feyaerts@realdolmen.com>
2017-12-12 13:46:41 +0100

    materialize-css toegevoegd

--- sportclub-frontend/src/app/app-routing.module.ts
+++ sportclub-frontend/src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {SportClubUserManagementComponent} from './sportclub-user-management/sportclub-user-management.component';
 import {SportClubEventsComponent} from './sportclub-events/sportclub-events.component';
 import {AppComponent} from "./app.component";
+import {SportclubEventCreationComponent} from "./sportclub-events/components/sportclub-event-creation/sportclub-event-creation.component";
 
 const routes: Routes = [
-
+  {
+    path: 'events/creation',
+    component: SportclubEventCreationComponent
+  },
   {
     path: 'user-management',
     component: SportClubUserManagementComponent
