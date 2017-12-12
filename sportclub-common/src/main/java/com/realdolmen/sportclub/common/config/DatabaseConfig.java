11c90de545e83fdcc214f952bd76406752b176e9
Ruth <ruth.reynders91@gmail.com>
2017-12-12 12:50:15 +0100

    finish h2db configs

--- sportclub-common/src/main/java/com/realdolmen/sportclub/common/config/DatabaseConfig.java
+++ sportclub-common/src/main/java/com/realdolmen/sportclub/common/config/DatabaseConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import javax.sql.DataSource;
 
 @Configuration
-@Profile("!local")
+@Profile("local")
 public class DatabaseConfig {
 
     @Bean
