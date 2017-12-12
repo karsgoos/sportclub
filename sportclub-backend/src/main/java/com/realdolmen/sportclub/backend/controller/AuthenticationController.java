e26d497808325b946abc6156bf11572f297bb8b9
Nico Rillaerts <Nico.Rillaerts@realdolmen.com>
2017-12-12 15:56:57 +0100

    Security startup

---
+++ sportclub-backend/src/main/java/com/realdolmen/sportclub/backend/controller/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.realdolmen.sportclub.backend.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    @PostMapping("/login")
+    public void attemptLogin() {
+
+    }
+}
