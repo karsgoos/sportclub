0eef90dea496edef1587cd7f048afd578f613236
Mathieu De Coster <mathieu.decoster@realdolmen.com>
2017-12-13 09:13:37 +0100

    Update and improve event validation in service layer

--- sportclub-events/src/main/java/com/realdolmen/sportclub/events/service/EventManagementServiceImpl.java
+++ sportclub-events/src/main/java/com/realdolmen/sportclub/events/service/EventManagementServiceImpl.java
@@ -3,12 +3,10 @@ package com.realdolmen.sportclub.events.service;
 import com.realdolmen.sportclub.common.entity.Event;
 import com.realdolmen.sportclub.events.exceptions.CouldNotCreateEventException;
 import com.realdolmen.sportclub.events.exceptions.CouldNotUpdateEventException;
+import com.realdolmen.sportclub.events.exceptions.InvalidEventException;
 import com.realdolmen.sportclub.events.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Service
 public class EventManagementServiceImpl implements EventManagementService {
@@ -21,8 +19,10 @@ public class EventManagementServiceImpl implements EventManagementService {
             throw new CouldNotCreateEventException(new IllegalArgumentException("Event cannot be null."));
         }
 
-        if (!isValid(event)) {
-            throw new CouldNotCreateEventException("Invalid event data.");
+        try {
+            validate(event);
+        } catch (InvalidEventException e) {
+            throw new CouldNotCreateEventException(e);
         }
 
         return repository.save(event);
@@ -34,8 +34,10 @@ public class EventManagementServiceImpl implements EventManagementService {
             throw new CouldNotUpdateEventException(new IllegalArgumentException("Event cannot be null."));
         }
 
-        if (!isValid(event)) {
-            throw new CouldNotUpdateEventException("Invalid event data.");
+        try {
+            validate(event);
+        } catch (InvalidEventException e) {
+            throw new CouldNotUpdateEventException(e);
         }
 
         return repository.save(event);
@@ -45,22 +47,42 @@ public class EventManagementServiceImpl implements EventManagementService {
      * An event is valid if it has a start date and end date in the future,
      * and if the end date is before the start date.
      *
-     * @param event
-     * @return
+     * @throws InvalidEventException If the event is not valid. The message of the
+     *                               exception will contain the reason validation failed.
      */
-    private boolean isValid(Event event) {
-        boolean valid = true;
-
+    private void validate(Event event) throws InvalidEventException {
         if (event.getStartDate() == null || event.getEndDate() == null) {
-            valid = false;
-        } else {
-            if (event.getStartDate().isBefore(LocalDateTime.now())) {
-                valid = false;
-            } else if (event.getEndDate().isBefore(event.getStartDate())) {
-                valid = false;
-            }
+            throw new InvalidEventException("Start and end date cannot be null.");
         }
-
-        return valid;
+        if (event.getEndDate().isBefore(event.getStartDate())) {
+            throw new InvalidEventException("End date can not be before the start date.");
+        }
+        if (event.getResponsibles() == null || event.getResponsibles().size() == 0) {
+            throw new InvalidEventException("The event should have at least one responsible.");
+        }
+        if (event.getEnrollments() == null) {
+            throw new InvalidEventException("Enrollments cannot be null.");
+        }
+        if (event.getAddress() == null) {
+            throw new InvalidEventException("Address cannot be null.");
+        }
+        if (event.getDeadline() == null) {
+            throw new InvalidEventException("Deadline cannot be null.");
+        }
+        if (event.getPrice() == null) {
+            throw new InvalidEventException("Price cannot be null.");
+        }
+        if (event.getName() == null) {
+            throw new InvalidEventException("Name cannot be null.");
+        }
+        if (event.getMaxParticipants() < 0) {
+            throw new InvalidEventException("Max participants must be greater than zero.");
+        }
+        if (event.getMinParticipants() < 1) {
+            throw new InvalidEventException("Min participants must be greater than one.");
+        }
+        if (event.getMinParticipants() > event.getMaxParticipants()) {
+            throw new InvalidEventException("Min participants must be smaller than or equal to max participants.");
+        }
     }
 }
