f8dbe31a34e881e40611e3e87790b8e20b2b4e86
Tim Ranson <Tim.Ranson@realdolmen.com>
2017-12-13 16:32:07 +0100

    Added mock User data

--- sportclub-backend/src/main/java/com/realdolmen/sportclub/backend/config/SecurityConfig.java
+++ sportclub-backend/src/main/java/com/realdolmen/sportclub/backend/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.realdolmen.sportclub.backend.config;
 import com.realdolmen.sportclub.backend.service.authentication.UserService;
 import com.realdolmen.sportclub.common.repository.RegisteredUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Order(SecurityProperties.BASIC_AUTH_ORDER-20)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     RegisteredUserRepository registeredUserRepository;
@@ -24,9 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().permitAll()
+        http
+                .formLogin()
                 .and()
-                .formLogin();
+                .authorizeRequests()
+                .anyRequest().authenticated()
+        ;
     }
 }
