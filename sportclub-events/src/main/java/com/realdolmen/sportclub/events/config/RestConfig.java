09aaea66fb77669a37fa180666564626afb326df
Jeroen Bosmans <jeroen.bosmans@realdolmen.com>
2017-12-14 10:50:38 +0100

    solved cross domain issues

---
+++ sportclub-events/src/main/java/com/realdolmen/sportclub/events/config/RestConfig.java
@@ -0,0 +1,33 @@
+package com.realdolmen.sportclub.events.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class RestConfig {
+
+    /**
+     * Allows our modules to use the javascript and webservices of this module.
+     * https://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
+     **/
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");   // core module
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+
+    }
+}
