a20246effd687c6262c5e00458d1c27bd080593e
Jonas Busseniers <j_busseniers@hotmail.com>
2017-12-14 12:12:53 +0100

    Deleted unused and non-existing component AdminTemplate

--- sportclub-frontend/src/app/app.module.ts
+++ sportclub-frontend/src/app/app.module.ts
@@ -6,13 +6,11 @@ import {AppRoutingModule} from './app-routing.module';
 import {SportClubUserManagementModule} from './sportclub-user-management/sportclub-user-management.module';
 import {SportClubEventsModule} from './sportclub-events/sportclub-events.module';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-import { AdminTemplateComponent } from './menu-balk/admin-template.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminTemplateComponent,
     MenuBarComponent
   ],
   imports: [
