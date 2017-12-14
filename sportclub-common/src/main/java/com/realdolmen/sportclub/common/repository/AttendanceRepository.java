76180068f1ad99d083bfc815e863cbf0cd31086e
Gust Wittevrongel <gust.wittevrongel@realdolmen.com>
2017-12-14 09:12:34 +0100

    repos for attendance and order

--- sportclub-common/src/main/java/com/realdolmen/sportclub/common/repository/AttendanceRepository.java
+++ sportclub-common/src/main/java/com/realdolmen/sportclub/common/repository/AttendanceRepository.java
@@ -1,4 +1,7 @@
 package com.realdolmen.sportclub.common.repository;
 
-public class AttendenceRepository {
+import com.realdolmen.sportclub.common.entity.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
 }
--- sportclub-common/src/main/java/com/realdolmen/sportclub/common/repository/OrderRepository.java
+++ sportclub-common/src/main/java/com/realdolmen/sportclub/common/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.realdolmen.sportclub.common.repository;
 
-public interface OrderRepository {
+import com.realdolmen.sportclub.common.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
 }
