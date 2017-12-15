f716a853abd291b39deaa7ddbc0bac0f57e29a77
Karsten Goossens <karsten.goossens@realdolmen.com>
2017-12-15 14:03:26 +0100

    Simple rest controller at the base of all api modules

---
+++ sportclub-points/src/main/java/com/realdolmen/sportclub/points/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.realdolmen.sportclub.points.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from the Points Service!";
+    }
+}
---
+++ sportclub-user-management/src/main/java/com/realdolmen/sportclub/usermanagement/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.realdolmen.sportclub.usermanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from the User Management Service!";
+    }
+}
