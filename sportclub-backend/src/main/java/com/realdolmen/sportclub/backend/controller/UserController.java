8ea76498d536e3e4046d63b08a808ec8c2040a61
Nico Rillaerts <Nico.Rillaerts@realdolmen.com>
2017-12-20 08:53:50 +0100

    Added API endpoint for adjusting user role

--- sportclub-backend/src/main/java/com/realdolmen/sportclub/backend/controller/UserController.java
+++ sportclub-backend/src/main/java/com/realdolmen/sportclub/backend/controller/UserController.java
@@ -2,15 +2,17 @@ package com.realdolmen.sportclub.backend.controller;
 
 import com.realdolmen.sportclub.backend.dao.UserPointsDao;
 import com.realdolmen.sportclub.common.entity.RegisteredUser;
+import com.realdolmen.sportclub.common.entity.Role;
 import com.realdolmen.sportclub.common.entity.authentication.AuthenticatedUser;
 import com.realdolmen.sportclub.common.repository.RegisteredUserRepository;
+import com.realdolmen.sportclub.common.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +23,30 @@ public class UserController {
     @Autowired
     RegisteredUserRepository registeredUserRepository;
 
+    @Autowired
+    RoleRepository roleRepository;
+
     @GetMapping("/register")
     public boolean register() {
         return true;
+    }
+
+    @PostMapping("/user/{id}/role")
+    // @PreAuthorize("hasAuthority('ADMINISTRATOR_PRIVILEGES')")
+    public ResponseEntity<Boolean> changeRole(@PathVariable("id") Long id, @RequestParam("role_id") Long role_id) {
+        RegisteredUser user;
+        Role role;
+
+        if ((user = registeredUserRepository.findOne(id)) != null) {
+             if ((role = roleRepository.findOne(role_id)) != null) {
+                 user.setRole(role);
+                 registeredUserRepository.save(user);
+
+                 return new ResponseEntity<>(true, HttpStatus.OK);
+             }
+        }
+
+        return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/user")
--- sportclub-common/src/main/java/com/realdolmen/sportclub/common/repository/RoleRepository.java
+++ sportclub-common/src/main/java/com/realdolmen/sportclub/common/repository/RoleRepository.java
@@ -3,6 +3,8 @@ package com.realdolmen.sportclub.common.repository;
 import com.realdolmen.sportclub.common.entity.Role;
 import com.realdolmen.sportclub.common.entity.Sportclub;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
